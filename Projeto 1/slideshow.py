4586ec360a1e013b81f735c2556c5e46e0d6da27
Mauricio Luis de Lorenzi <luis_mauricio_13_@hotmail.com>
2019-03-16 16:20:00 -0300

    slideshow is done

--- Projeto 1/slideshow.py
+++ Projeto 1/slideshow.py
@@ -1,39 +1,94 @@
 import cv2
 import os
+import numpy
 
-imagesPath = "images"
-images = []
+IMAGES_PATH = "images"  # Root where the images are
+IMAGES = []  # Images path array
+SLIDE_QUEUE = []  # The slide images queue
+CHANGE_IMAGE = 2000  # 2 seconds to change between images
+TRANSITION = 50  # 50 mili seconds of transition  int the addWeighted loop
+BORDER_SIZE = 20  # 20px for size of the image border
+WATER_MARK_PATH = "water_mark.png"  # Path of the water mark
 
-for file in os.listdir(imagesPath): 
-    images.append(os.path.join(imagesPath, file))
+# Resize the image to 100 x 100
+def Resize(image):
+    return cv2.resize(image, (400, 300), interpolation=cv2.INTER_AREA)
 
-print(images)
+# Add border 20 px into the image
+def AddBorder(image):
+    return cv2.copyMakeBorder(image, BORDER_SIZE, BORDER_SIZE, BORDER_SIZE, BORDER_SIZE, cv2.BORDER_CONSTANT, value=[255, 255, 255, 255])
 
-""" img = cv2.imread('lena.jpg', cv2.IMREAD_GRAYSCALE)
+# Add a water mark into the image
+def AddWaterMark(image):
+    waterMarkImage = cv2.imread(WATER_MARK_PATH)
 
-cv2.imshow('Img', img)
-cv2.imwrite('lena_graysclae.jpg', img)
+    # This point represent the height point
+    xInit = image.shape[0] - BORDER_SIZE - waterMarkImage.shape[0]
+    xEnd = image.shape[0] - BORDER_SIZE
 
-cv2.waitKey()
-cv2.destroyAllWindows() 
+    # This point represent the width point
+    yInit = BORDER_SIZE
+    yEnd = waterMarkImage.shape[1] + BORDER_SIZE
 
-----------------
+    # Getting a region of interest
+    roi = image[xInit:xEnd, yInit:yEnd]
+  
+    # Adding alpha to the roi and the water mark image
+    roi = cv2.addWeighted(roi, 0.5, waterMarkImage, 0.5, 0)
+    
+    # Putting the roi image over the original slide image
+    image[xInit:xEnd, yInit:yEnd] = roi
+    
+    return image
 
-import numpy as np
-import cv2
+# Main
 
-i = cv2.imread('image.jpg')
-img = np.array(i, dtype=np.float)
-img /= 255.0
-cv2.imshow('img',img)
-cv2.waitKey(0)
+# Get all the images from the path
+for file in os.listdir(IMAGES_PATH):
+    IMAGES.append(os.path.join(IMAGES_PATH, file))
 
-#pre-multiplication
-a_channel = np.ones(img.shape, dtype=np.float)/2.0
-image = img*a_channel
+SLIDE_QUEUE = IMAGES.copy()
 
-cv2.imshow('img',image)
-cv2.waitKey(0)
+# Get and remove the first image of the image array
+img = cv2.imread(SLIDE_QUEUE.pop(0), cv2.IMREAD_UNCHANGED)
+
+# Get and remove the first image of the image array
+img2 = cv2.imread(SLIDE_QUEUE.pop(0), cv2.IMREAD_UNCHANGED)
+
+while True:
+
+    if len(SLIDE_QUEUE) <= 0:
+        SLIDE_QUEUE = IMAGES.copy()
+
+    # Resize Image
+    imgResized = Resize(img)
+    img2Resized = Resize(img2)
+
+    # Add Border
+    imgBordered = AddBorder(imgResized)
+    img2Bordered = AddBorder(img2Resized)
+
+    # Add Water Mark
+    imgWithWaterMark = AddWaterMark(imgBordered)
+    img2WithWaterMark = AddWaterMark(img2Bordered)
+
+    cv2.imshow('Slides', imgBordered)
+
+    # Check if q key is pressed and wait 2 seconds before continue
+    if cv2.waitKey(CHANGE_IMAGE) & 0xFF == ord('q'):
+        break
+
+    # Do the transition
+    for alpha in numpy.arange(0, 1, 0.05):
+        # Show image
+        cv2.imshow('Slides', cv2.addWeighted(imgWithWaterMark, 1-alpha, img2WithWaterMark, alpha, 0))
+        
+        # Wait 62 ms                
+        cv2.waitKey(TRANSITION)    
+
+    # throw away the first image
+    img = img2
+    # Get and remove the first image of the image array
+    img2 = cv2.imread(SLIDE_QUEUE.pop(0), cv2.IMREAD_UNCHANGED)
+
 cv2.destroyAllWindows()
-
-"""
