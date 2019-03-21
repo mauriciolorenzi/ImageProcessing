83dd064a0b08a32e54693365370e5d0370810ddd
Mauricio Luis de Lorenzi <luis_mauricio_13_@hotmail.com>
2019-03-21 20:51:37 -0300

    started the project 2

---
+++ Projeto 2/erosion.py
@@ -0,0 +1,28 @@
+#Bruno Sampaio RA: 140004
+#Mauricio Luis de Lorenzi RA: 141269
+
+import cv2
+import numpy
+import pprint
+
+DATA = numpy.array(
+    [ 
+        [1, 1, 1, 0, 1, 1, 1, 1, 1, 1], 
+        [1, 1, 1, 0, 1, 1, 1, 1, 1, 1], 
+        [1, 1, 0, 0, 1, 1, 1, 1, 1, 1], 
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1], 
+        [0, 0, 0, 0, 1, 1, 1, 0, 1, 1], 
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 0], 
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 0], 
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 0], 
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 0] 
+    ], dtype = numpy.uint8 )
+
+kernel = numpy.ones((3,3), numpy.uint8)
+
+erode  = cv2.erode(DATA, kernel)    
+
+pprint.pprint(DATA)
+print("\n")
+pprint.pprint(erode)
