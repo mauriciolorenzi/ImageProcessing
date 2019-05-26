4955e3b543d3348b12afd83ff4fdcf90e357258e
MauricioLorenzi <luis_mauricio_13_@hotmail.com>
2019-05-26 19:40:35 -0300

    Removed some comments, isn't the best algorithm because it isn't detecting two dices together and is failing to detect the dice's balls because of the light on the balls

--- Projeto 3/face_dice_detection_on_video.py
+++ Projeto 3/face_dice_detection_on_video.py
@@ -30,12 +30,9 @@ def DetectDiceFaceNumber(dices):
 
         # Do the binarization of the gray format image
         _,thresh = cv2.threshold(dices_gray, 127, 255, cv2.THRESH_BINARY)
-        #_,thresh = cv2.threshold(dices_bgr, 127, 0, cv2.THRESH_BINARY_INV)
 
         # Apply Opening morphological transformation (erosion followed by dilation) to remove all noises of the image
         thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, KERNEL)
-        #thresh = cv2.dilate(thresh, numpy.ones((5, 5), numpy.uint8), KERNEL)
-        cv2.imshow("thresh", thresh)
       
         # Find the contours (dices' faces)
         contours,_ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -47,34 +44,26 @@ def DetectDiceFaceNumber(dices):
         for contour in contours :
                 # Get the respective x and y of start and end point
                 (x,y,w,h) = cv2.boundingRect(contour)
+
                 # Get only the dice's face image
                 roi = dices[y : y + h, x : x + w].copy()
 
                 # Get the roi area size
                 roi_area = w * h
 
+                # Checking if the image has the correct size
                 if roi_area > MIN_RANGE and roi_area < MAX_RANGE:
-                        # Getting the roi in grayscale
-                        #roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-                        # Do the binarization of the grayscale roi
-                       # _, roi = cv2.threshold(roi, 127, 255, cv2.THRESH_BINARY)
-                        #roi = cv2.morphologyEx(roi, cv2.MORPH_OPEN, DICE_KERNEL)
                         # Get the keypoints
                         keypoints = detector.detect(roi)
                         if len(keypoints) > 0:
-                                cv2.imshow("roi", roi)
                                 # X start position of the number
-                                #numberX = int(x + (w / 2))
                                 numbersX.append(int(x + (w / 2)))
 
                                 # Y start position of the number
-                                #numberY = int(y + (h / 2))
                                 numbersY.append(int(y + (h / 2)))
 
-                                # Count the number of face's balls
-                                for marker in keypoints:
-                                        # Increment the number of balls found in dices' face                                
-                                        FACE_BALLS_NUMBER += 1
+                                # Get the number of face's balls                                
+                                FACE_BALLS_NUMBER = len(keypoints)
 
                                 # Append the dice's face balls number to an array
                                 if FACE_BALLS_NUMBER != 0:
@@ -83,18 +72,14 @@ def DetectDiceFaceNumber(dices):
                                 # Restart the count of a face's number 
                                 FACE_BALLS_NUMBER = 0  
                                 #cv2.rectangle(dices, (x,y), (x+w,y+h), (0,0,255), 2)
-                        else:
-                                cv2.destroyWindow("roi")             
         
         return FACE_BALLS_NUMBERS, numbersX, numbersY
 
 while DICES_VIDEO.isOpened() :        
         ret, frame = DICES_VIDEO.read()
-        #COUNT += 1
 
-        #if COUNT == 10:
-        #        COUNT = 0
         FACE_BALLS_NUMBERS, numbersX, numbersY = DetectDiceFaceNumber(frame)
+
         # Print the number of balls on the right face]
         if len(FACE_BALLS_NUMBERS) > 0:
                 for i in range(len(FACE_BALLS_NUMBERS)):
