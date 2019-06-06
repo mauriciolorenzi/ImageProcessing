72e4ea906655cb08e0333128e9578fbacb7e11c8
MauricioLorenzi <luis_mauricio_13_@hotmail.com>
2019-06-05 23:12:14 -0300

    Finished the chroma key project

--- Projeto 5/chroma_key.py
+++ Projeto 5/chroma_key.py
@@ -1,30 +1,41 @@
-#
+#Douglas Barioni Amorim   RA: 130081
 #Mauricio Luis de Lorenzi RA: 141269
 
 import cv2
+import matplotlib.pyplot as plt
 import numpy
 
 CHROMA_KEY_VIDEO = cv2.VideoCapture('chroma_key_video.mp4')
-BACKGROUND_VIDEO = cv2.VideoCapture('chroma_key_video.mp4')
-MOG = cv2.createBackgroundSubtractorMOG2()
-KERNEL = numpy.ones((11, 11), numpy.uint8)
+BACKGROUND_VIDEO = cv2.VideoCapture('background_video.mp4')
 
 while CHROMA_KEY_VIDEO.isOpened() and BACKGROUND_VIDEO.isOpened():        
         ret, frame = CHROMA_KEY_VIDEO.read()
-        ret_backgorund, frame_background = BACKGROUND_VIDEO.read()
+        ret_background, frame_background = BACKGROUND_VIDEO.read()
 
-        if(ret and ret_backgorund) : 
-                frame2 = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2GRAY)
-                mask = MOG.apply(frame2)
+        if(ret and ret_background) : 
+                copy_frame = numpy.copy(frame)
+                copy_frame = cv2.cvtColor(copy_frame, cv2.COLOR_BGR2RGB)
+                frame_background = cv2.cvtColor(frame_background, cv2.COLOR_BGR2RGB)
 
-                gaussianBlur = cv2.GaussianBlur(mask, (11, 11), 3.5)
-                ret,thresh = cv2.threshold(gaussianBlur, 10, 255, cv2.THRESH_BINARY)
-                cv2.imshow("Real", frame)
+                lower_green = numpy.array([0, 100, 0])    
+                upper_green = numpy.array([100, 255, 70])
 
-                #Remove the background        
-                im = frame2 - thresh
+                mask = cv2.inRange(copy_frame, lower_green, upper_green)
 
-                cv2.imshow("chroma key video", cv2.cvtColor(im, cv2.COLOR_GRAY2BGR))
+                masked_frame = numpy.copy(copy_frame)
+                masked_frame[mask != 0] = [0, 0, 0]
+
+                # Crop the image
+                crop_background = frame_background[0:720, 0:1280]
+
+                # Mask the cropped background
+                crop_background[mask == 0] = [0, 0, 0]
+
+                # Add the two images together to create a complete image
+                final_image = cv2.cvtColor(crop_background + masked_frame, cv2.COLOR_RGB2BGR)
+
+                # Show the result
+                cv2.imshow('chroma key video', final_image)                
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
